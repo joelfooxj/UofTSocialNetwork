27bf963b8fb3bb01d78d2ed974137148e4932d0d
Viktar Chyhir <vchigir12@gmail.com>
2020-02-19 19:37:55 -0500

    Update main file to make sure the app actually runs

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+
 
 import LogInPage from './react-components/LogInPage';
 
 class App extends React.Component{
+
+  state = {
+    signedIn: false,
+    permission: 0, // 0 - reg user, 1 - admin
+    execOf: [],
+    accountId: -1
+  }
+
+  changeSignInStatus(val, id, perm, clubs){
+    this.setState({
+      signedIn: val,
+      accountId: id,
+      permission: perm,
+      execOf: clubs
+    })
+  }
+
   render(){
+    console.log(this.state)
     return (
-      <div className="App">
-        <LogInPage/>
-      </div>
+      <BrowserRouter>
+          <Switch> { /* Similar to a switch statement - shows the component depending on the URL path */ }
+            { /* Each Route below shows a different component depending on the exact path in the URL  */ }
+            <Route exact path='/' render={() => 
+                            (<LogInPage changeSignInStatus={this.changeSignInStatus.bind(this)}/>)}/>
+          </Switch>
+        </BrowserRouter>
     );
   }
 }
