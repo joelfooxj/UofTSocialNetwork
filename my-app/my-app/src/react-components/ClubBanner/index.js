d5a1a9a6f69f992ed2d5400d3a1d4c0be48e739e
nikita-sh <shumeiko.nikita@gmail.com>
2020-02-21 00:17:28 -0500

    Added timeline stub

--- my-app/my-app/src/react-components/ClubBanner/index.js
+++ my-app/my-app/src/react-components/ClubBanner/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './style.css';
-import ClubInfo from "../ClubInfo/index.js"
+import ClubInfo from "../ClubInfo"
 
 class ClubProfileBanner extends React.Component {
     render() {
--- my-app/my-app/src/react-components/ClubProfilePage/index.js
+++ my-app/my-app/src/react-components/ClubProfilePage/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import './style.css';
+import ClubProfileBanner from "../ClubBanner";
+import ClubTimeline from "../ClubTimeline";
 
 class ClubProfilePage extends React.Component {
     render() {
         return(
             <div>
                 This is placeholder.
+                <ClubProfileBanner profileImage="" bannerImage=""/>
+                <ClubTimeline />
             </div>
         )
     }
