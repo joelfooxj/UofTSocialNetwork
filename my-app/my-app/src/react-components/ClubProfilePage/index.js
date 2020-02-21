71b6a18dcb84965d91e9a8693ec5aa43801dcf32
nikita-sh <shumeiko.nikita@gmail.com>
2020-02-21 11:59:38 -0500

    Added placeholder timeline

--- my-app/my-app/src/react-components/ClubProfilePage/index.js
+++ my-app/my-app/src/react-components/ClubProfilePage/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import './style.css';
 
+import ClubProfileBanner from "../ClubBanner";
+import ClubTimeline from "../ClubTimeLine";
+
 class ClubProfilePage extends React.Component {
     render() {
         return(
             <div>
-                This is placeholder.
+                <ClubProfileBanner profileImage="" bannerImage=""/>
+                <ClubTimeline/>
             </div>
         )
     }
 }
+
+export default ClubProfilePage;
