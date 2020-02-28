63dab8bb64a4c0bfc5d9ef34b5a77b85f459c40a
joelfooxj <joelfooxj@gmail.com>
2020-02-28 14:16:49 -0500

    - Update tempInfo.js - Completed basic BrowseAllClubs component

---
+++ my-app/my-app/src/react-components/BrowseAllClubs/index.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import './index.css';
+import {Container, TextField, Button, List, ListItem, ListItemText, ListItemSecondaryAction, Paper } from '@material-ui/core';
+
+
+class BrowseAllClubs extends React.Component {
+  constructor(props){
+    super(props); 
+    this.state={
+      tempData: props.tempData, 
+      allClubs: props.allClubs,
+      displayedClubs: props.allClubs
+    }
+  }
+
+  filterClubs = e => { 
+    this.setState({
+      displayedClubs: e.target.value === '' ?
+      this.state.allClubs : this.state.allClubs.filter(club => club.name.includes(e.target.value))
+    });
+  }
+
+  goToClub = clubID => {
+    alert("Going to profile page of: club " + clubID);
+  }
+
+  joinRequest = e => {
+    console.log(e.currentTarget.id); 
+    let getClub = this.state.allClubs.filter(club => club.clubID === parseInt(e.currentTarget.id))[0];
+    getClub.requests.push(this.props.currentUser.userID);
+    this.setState({ allClubs: this.props.allClubs });
+  }
+
+  render(){
+    return (
+      <div> 
+        <Container maxWidth="xs">
+          <h1 className="mainTitle"> CLUBS </h1> 
+          <form onChange={this.filterClubs}> 
+            <TextField 
+            label="Search clubs by name" 
+            variant="filled"
+            color="primary"
+            size="small"
+            fullWidth/>
+          </form>
+          <List dense={true}> 
+            {
+              this.state.displayedClubs.map(club => {
+                let disabledState = false; 
+                let buttonText = ''; 
+                if (club.members.includes(this.props.currentUser.userID)) {
+                  disabledState = true; 
+                  buttonText = 'Joined'; 
+                } else if (club.requests.includes(this.props.currentUser.userID)) {
+                  disabledState = true; 
+                  buttonText = 'Requested'
+                } else {
+                  disabledState = false; 
+                  buttonText = 'Request to join';
+                }
+                return (
+                <Paper elevation={0} variant='outlined' key={club.clubID}> 
+                  <ListItem button onClick={() => this.goToClub(club.clubID)} variant="outlined">
+                    <ListItemText
+                    primary={club.name}/>
+                      <ListItemSecondaryAction>
+                        {
+                          <Button 
+                            id={club.clubID}
+                            size="small"
+                            edge="end" 
+                            aria-label="join" 
+                            variant="outlined"
+                            color="primary"
+                            disabled={disabledState}
+                            onClick={this.joinRequest}
+                            >
+                            {buttonText}
+                          </Button>
+                        }
+                      </ListItemSecondaryAction>
+                  </ListItem>
+                </Paper>
+                )})
+            }
+          </List>
+        </Container> 
+      </div>
+    )
+  }
+}
+
+export default BrowseAllClubs; 
--- my-app/my-app/src/tempInfo.js
+++ my-app/my-app/src/tempInfo.js
@@ -4,9 +4,12 @@
 // POSTS
 
 const Posts = [ 
-  {title: "first!", content:"Lorem ipsum dolor sit amet", author:'joel', date:'20-02-2020'}, 
-  {title: "second!", content:"it, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nes", author:'victor', date:'20-03-2020'},
-  {title: "first!", content:"odi consequatur? Quis autem vel eu", author:'nikita', date:'20-04-2020'}
+  {postID: 1, title: "first!", content:"Lorem ipsum dolor sit amet", authorID:1, date:'20-02-2020'}, 
+  {postID: 2, title: "second!", content:"it, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nes", authorID:2, date:'20-03-2020'},
+  {postID: 3, title: "third!", content:"odi consequatur? Quis autem vel eu", authorID:1, date:'20-04-2020'}, 
+  {postID: 4, title: "fourth!", content:"odi consequafasdfastfur? Qfaduis autem vel eu", authorID:3, date:'20-04-2020'}, 
+  {postID: 5, title: "fifth!", content:"odi efa aeraberbaaconsequatur? Quis autem vel eu", authorID:4, date:'20-04-2020'}, 
+  {postID: 6, title: "sixth!", content:"odi consequavaetrasur? Qsruis aaabrawrutem vel eu", authorID:4, date:'20-04-2020'}
 ]
 
 const Users = [
@@ -19,9 +22,9 @@ const Users = [
 
 // See that clubs hold a list of users who have exec permissions
 const Clubs = [
-  {clubID: 1, name: 'csc309', execs:['joel', 'nikita'], posts:[Posts[0]]},
-  {clubID: 2, name: 'uoft', execs:['zhan'], posts:[Posts[1]]},
-  {clubID: 3, name: 'team11', execs:['joel', 'victor'], posts:[Posts[2]]}
+  {clubID: 1, name: 'csc309', execs:[1,4], posts:[1,2], requests:[2], members:[1,3]},
+  {clubID: 2, name: 'uoft', execs:[3], posts:[3,4], requests:[2,4], members:[1,3]},
+  {clubID: 3, name: 'team11', execs:[2,3], posts:[5,6], requests:[1], members:[2,3]}
 ]
 
-export default [Users, Clubs]; 
+export default {Users, Clubs, Posts}; 
