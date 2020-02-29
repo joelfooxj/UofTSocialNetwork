339742fc12e3ed270de221769883b5dd60933201
Viktar Chyhir <vchigir12@gmail.com>
2020-02-29 10:02:48 -0500

    Quick fix to account id assignment

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -86,7 +86,7 @@ class App extends React.Component{
   }
 
   createAccount = (username, permissions, password, firstName, lastName, email) => {
-    const newAcc = new this.Account(username, permissions, [], this.state.accounts.length + 1, password, firstName, lastName, email)
+    const newAcc = new this.Account(username, permissions, [], this.state.accounts[this.state.accounts.length - 1].id + 1, password, firstName, lastName, email)
     const accs = this.state.accounts
     accs.push(newAcc)
     this.setState({
