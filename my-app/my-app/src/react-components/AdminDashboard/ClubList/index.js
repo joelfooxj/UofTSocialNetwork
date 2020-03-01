5888d813c8136e5f77cb31604ee140e2026b96fa
joelfooxj <joelfooxj@gmail.com>
2020-03-01 14:32:58 -0500

    - Linked User and Club item buttons to their respective pages - Added LOGOUT button in AdminDashboard

--- my-app/my-app/src/react-components/AdminDashboard/ClubList/index.js
+++ my-app/my-app/src/react-components/AdminDashboard/ClubList/index.js
@@ -2,6 +2,8 @@ import React from '../../../../node_modules/react';
 import { Grid, List, ListItem, ListItemText, IconButton, ListItemSecondaryAction, Paper } from '../../../../node_modules/@material-ui/core'
 import DeleteIcon from '../../../../node_modules/@material-ui/icons/Delete' 
 
+import { Link } from '../../../../node_modules/react-router-dom'
+
 const ClubList = props => {
 		
     const clubs = props.clubsArr;
@@ -11,19 +13,29 @@ const ClubList = props => {
             <Grid container spacing={2}>
                     <Grid item xs={12} md={6}> 
 											<List dense={true}> 
-													{clubs.map(club => 
-													<Paper elevation={0} variant='outlined' key={club.clubID} >
-                            <ListItem button onClick={() => props.onClick('club', club.clubID)}> 
-                                <ListItemText
-                                  primary={club.name}
-                                />
-                                <ListItemSecondaryAction>
-                                  <IconButton edge="end" aria-label="delete" onClick={() => props.onDelete('club', club.clubID)}>
-                                    <DeleteIcon fontSize="small" color="primary"/>
-                                  </IconButton>
-                                </ListItemSecondaryAction>
-                            </ListItem> 
-                          </Paper>		
+                          {clubs.map(club => 
+                          <Link to={{
+                            pathname: "/ClubProfilePage", 
+                            state: {
+                              club: club
+                            }
+                          }}
+                          style={{ textDecoration:'none' }}
+                          key={club.clubID}
+                          >
+                            <Paper elevation={0} variant='outlined'  >
+                              <ListItem button> 
+                                  <ListItemText
+                                    primary={club.name}
+                                  />
+                                  <ListItemSecondaryAction>
+                                    <IconButton edge="end" aria-label="delete" onClick={() => props.onDelete('club', club.clubID)}>
+                                      <DeleteIcon fontSize="small" color="primary"/>
+                                    </IconButton>
+                                  </ListItemSecondaryAction>
+                              </ListItem> 
+                            </Paper>		
+                          </Link>
 													)}
 											</List> 
                     </Grid> 
