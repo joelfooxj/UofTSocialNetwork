5c5fed5495947932e657c6f09669bdd0f55bb187
Zhan Lu <zhanlu0523@gmail.com>
2020-03-03 21:29:08 -0500

    update style

--- my-app/my-app/src/react-components/BrowseAllClubs/index.js
+++ my-app/my-app/src/react-components/BrowseAllClubs/index.js
@@ -47,7 +47,7 @@ class BrowseAllClubs extends React.Component {
         <Navbar changeSignInStatus={this.props.changeSignInStatus} logoPic='https://pngimage.net/wp-content/uploads/2018/06/logo-placeholder-png-6.png' 
           status={true} user={this.props.user}>
         </Navbar>
-        <Container maxWidth="xs">
+        <Container id="container" maxWidth="xs">
           <h1 className="mainTitle"> CLUBS </h1> 
           <form onChange={this.filterClubs}> 
             <TextField 
