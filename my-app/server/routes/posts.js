2d2f81a7d8a493967b0978d50d546b10ef2baf0b
nikita-sh <shumeiko.nikita@gmail.com>
2020-03-23 18:55:54 -0400

    Finished post routes and created wrapper functions for them

--- my-app/server/routes/posts.js
+++ my-app/server/routes/posts.js
@@ -2,8 +2,6 @@ const express = require('express')
 const router = express.Router()
 const { Post } = require('../../models/Post.js')
 const { ObjectID } = require('mongodb')
-const fs = require('fs');
-const { sessionChecker} = require('../mainServer')
 
 /* post request for adding a new post
  *
@@ -11,7 +9,7 @@ const { sessionChecker} = require('../mainServer')
  *     - posterID: id of poster
  *     - content: post content
 */
-router.post('/makePost', (req, res) => {
+router.post('/create', (req, res) => {
     if (!req.body.posterID || !req.body.content) {
         res.status(400).send()
         return;
@@ -44,7 +42,7 @@ router.post('/makePost', (req, res) => {
  * req body expects:
  *     - nothing
 */
-router.get('/getPost/:id', (req, res) => {
+router.get('/get/:id', (req, res) => {
     const id = req.params.id
 
     if (!ObjectID.isValid(id)) {
@@ -60,37 +58,12 @@ router.get('/getPost/:id', (req, res) => {
     })
 })
 
-/* route for getting posts based on a set of ids
- *
- * req body expects:
- *     - ids: array of ids to get posts from
-*/
-router.get('/getPostSet', (req, res) => {
-    const ids = req.body.ids;
-
-    for (let i = 0; i < ids.length; i++) {
-        if (!ObjectID.isValid(ids[i])) {
-            res.status(400).send()
-            return;
-        }
-    }
-
-    Post.find({"posterID": {$in: ids.map(x => {
-        new ObjectID(x)
-    })}}).then((posts) => {
-        res.status(200).send(posts)
-    }).catch((error) => {
-        console.log(error)
-        res.status(400).send()
-    })
-})
-
 /* delete a post based on id
  *
  * req body expects:
  *     - nothing
 */
-router.delete('/removePost/:id', (req, res) => {
+router.delete('/remove/:id', (req, res) => {
     const id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
@@ -102,7 +75,7 @@ router.delete('/removePost/:id', (req, res) => {
         if (!post) {
             res.status(404).send()
         } else {
-            res.status(200).send()
+            res.status(200).send(post)
         }
     }).catch((err) => {
         console.log(err)
@@ -113,40 +86,32 @@ router.delete('/removePost/:id', (req, res) => {
 /* patch request for updating a post
  *
  * req body expects:
- *     - posterID: id of poster
- *     - any previous elements in the post (updated or not)
+ *     - attr: attribute we wish to update
+ *     - nVal: new value of attribute
 */
-router.patch('/updatePost/:id', (req, res) => {
+router.patch('/update/:id', (req, res) => {
     const id = req.params.id
-
-    if (!ObjectID.isValid()) {
+    if (!ObjectID.isValid(id)) {
+        res.status(400).send()
+        return;
+    } else if (req.body.propertyName === "" || req.body.propertyVal === "") {
         res.status(400).send()
         return;
     }
 
-    Post.findById(id).then((post) => {
-        if (!post) {
-            res.status(404).send()
+    const update = {
+        [req.body.attr]: req.body.nVal
+    }
+
+    Post.findOneAndUpdate({_id: id}, update).then((result) => {
+        if (!result) {
+            res.status(500).send()
         } else {
-            if (req.body.title) {
-                post.title = req.body.title
-            }
-
-            if (req.body.content) {
-                post.title = req.body.content
-            }
-
-            if (req.body.location) {
-                post.location = req.body.location
-            }
-
-            return post.save()
+            res.status(200).send(result)
         }
-    }).then((res) => {
-        res.status(200).send()
-    }).catch((err) => {
-        console.log(err)
-        res.status(400).send()
+    }).catch((error) => {
+        console.log(error)
+        res.status(500).send()
     })
 })
 
