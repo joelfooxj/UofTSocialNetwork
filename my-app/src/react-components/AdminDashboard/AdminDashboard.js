05ed383b9385b07d31a89b8049e27727bb3f16be
joelfooxj <joelfooxj@gmail.com>
2020-03-26 13:23:06 -0400

    Updated AdminDashboard.js with DB functions

--- my-app/src/react-components/AdminDashboard/AdminDashboard.js
+++ my-app/src/react-components/AdminDashboard/AdminDashboard.js
@@ -7,36 +7,55 @@ import { Button } from '@material-ui/core';
 import { Link } from '../../../node_modules/react-router-dom'
 import Navbar from '../Navbar'
 import { withRouter } from 'react-router-dom';
+import { getAllClubs } from '../../actions/clubActions'
+
 
 class AdminDashboard extends React.Component {
 	// props should contain Accounts and Clubs
     constructor(props){
 			super(props); 
 			this.state={
-				accounts: props.accounts, 
-				clubs: props.clubs,
+				accounts: [],
+				clubs: [],
+				posts: [] 
 			}	
 		}
 		
 		componentDidMount(){
-			this.fetchData(); 
+			this.fetchClubs(); 
+			this.fetchAccounts();
+			this.fetchPosts();  
 		}
 
-		fetchData(){ 
-			//TODO: fetch data from the DB here 
+		fetchClubs(){ 
+			try {
+				const allClubs = getAllClubs(); 
+				this.setState({ clubs: allClubs });
+			} catch (error) {
+				alert(`${error}: There was an error retrieving all clubs`); 
+			}
 		}
 
-		deleteObject = (inType, inID)  => {
-			//TODO: delete object from database
-			this.setState(inType === 'user' ? 
-			{accounts: this.state.accounts.filter(user => user.id != inID)} : 
-			{clubs: this.state.clubs.filter(club => club.clubID != inID)});
+		fetchAccounts(){
+			try {
+				const allAccounts = getAllAccounts(); 
+				this.setState({ accounts: allAccounts });
+			} catch (error) {
+				alert(`${error}: There was an error retrieving all accounts`); 
+			}
+		}
+
+		fetchPosts(){
+			try {
+				const allPosts = getAllPosts(); 
+				this.setState({ posts: allPosts });
+			} catch (error) {
+				alert(`${error}: There was an error retrieving all posts`); 
+			}
 		}
 
     render(){
-				const totalPosts = this.state.clubs.map(club => club.posts.length).reduce((c, p) => c + p, 0)
-				const { changeSignInStatus, user, accounts, clubs } = this.props;
-				// const totalPosts = this.state.clubs.map((p, club) => p + club.posts.length, 0)
+				const { changeSignInStatus, user } = this.props;
         return(
         	<div>
         	<Navbar logoPic='https://pngimage.net/wp-content/uploads/2018/06/logo-placeholder-png-6.png' 
@@ -46,17 +65,17 @@ class AdminDashboard extends React.Component {
                 <AdminStats 
                     numUsers={this.state.accounts.length}
                     numClubs={this.state.clubs.length}
-                    numPosts={totalPosts}
+                    numPosts={this.state.posts.length}
                 />
                 <UserList
 								usersArr={this.state.accounts}
-								onClick={this.goToObject}
+								onClick={this.goToAccount}
 								/>
                 <ClubList
 								clubsArr={this.state.clubs}
 								onDelete={this.deleteObject}
 								/> 
-								<Link to="/" style={{ textDecoration:'none' }}> 
+								<Link to="/" className="notUnderlined"> 
 									<Button
 										size="small"
 										edge="end" 
@@ -68,7 +87,7 @@ class AdminDashboard extends React.Component {
 									</Button>
 								</Link>
             </div>
-            </div>
+          </div>
         );
     }
     
