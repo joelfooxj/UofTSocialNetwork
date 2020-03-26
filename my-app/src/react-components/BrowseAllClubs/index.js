0ff14daa18075c1bbfb7bceb4e4b910f82848c15
joelfooxj <joelfooxj@gmail.com>
2020-03-25 23:01:47 -0400

    Updated BrowseAllClubs with DB calls

--- my-app/src/react-components/BrowseAllClubs/index.js
+++ my-app/src/react-components/BrowseAllClubs/index.js
@@ -3,20 +3,40 @@ import './index.css';
 import {Container, TextField, Button, List, ListItem, ListItemText, ListItemSecondaryAction, Paper } from '@material-ui/core';
 import { withRouter, Link } from 'react-router-dom';
 import Navbar from '../Navbar'
+import { getAllClubs, updateClub } from '../../actions/clubActions';
+
 
 
 class BrowseAllClubs extends React.Component {
   constructor(props){
     super(props); 
     this.state={
-      tempData: props.tempData, 
-      allClubs: props.allClubs,
-      displayedClubs: props.allClubs
+      allClubs: [], 
+      displayedClubs: []
     }
   }
 
   //THESE SHOULD NOW BE SUPPLEMENTED BY OUR DB FUNCTIONS
   ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  // user this.props.userInfo for the currently logged in user
+  
+  componentDidMount(){
+    console.log("starting component"); 
+    try {
+      const retClubs = getAllClubs(); 
+      this.setState({
+        allClubs: retClubs, 
+        displayedClubs: retClubs
+      })
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+
+
   filterClubs = e => { 
     this.setState({
       displayedClubs: e.target.value === '' ?
@@ -24,23 +44,42 @@ class BrowseAllClubs extends React.Component {
     });
   }
 
-  goToClub = clubID => {
-    let target = null
-    target = this.state.allClubs.find(club => club.clubID == clubID);
+  // TODO: make sure that this links to the correct page.
+  // Don't actually every use this...
+  // goToClub = clubID => {
+  //   let target = null
+  //   target = this.state.allClubs.find(club => club._id == clubID);
 
-    if (target) {
-      const {history} = this.props; 
-      history.push(target.link);
-    } else {
-      alert("Something went wrong.")
-    }
-  }
+  //   if (target) {
+  //     const {history} = this.props; 
+  //     history.push(`/clubs/${target._id}`);
+  //   } else {
+  //     alert("Unable to find club link.")
+  //   }
+  // }
 
   joinRequest = e => {
-    console.log(e.currentTarget.id); 
-    let getClub = this.state.allClubs.find(club => club.clubID === parseInt(e.currentTarget.id));    
-    getClub.requests.push(this.props.currentUserID);
-    this.setState({ allClubs: this.props.allClubs });
+    console.log(e.currentTarget._id); 
+    let allClubsCopy = [...this.state.allClubs];
+    let getClub = allClubsCopy.find(club => club._id === parseInt(e.currentTarget._id));
+    let retRequested = getClub.requested;
+    if (retRequested.includes(this.props.userInfo._id)){
+      alert("You have already joined this club.") // THERE IS A BUG IF THIS SHOWS UP
+    } else {
+      retRequested.push(this.props.userInfo._id);
+      try {
+        if (updateClub(getClub._id, requested, retRequested) !== 200){
+          alert(`Failed to update club ${getClub.name}`)
+        }
+      } catch (error) {
+        alert(error);
+        alert(`Failed to update club ${getClub.name}`);
+        retRequested.pop(); 
+      }
+      this.setState({allClubs: allClubsCopy});
+    }
+
+    
   }
   ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -60,15 +99,18 @@ class BrowseAllClubs extends React.Component {
             size="small"
             fullWidth/>
           </form>
-          <List dense={true}> 
+          {
+            this.state.allClubs.length === 0 ? 
+            <h2> There are no clubs. </h2> :
+            <List dense={true}> 
             {
               this.state.displayedClubs.map(club => {
                 let disabledState = false; 
                 let buttonText = ''; 
-                if (club.members.includes(this.props.currentUserID)) {
+                if (club.members.includes(this.props.userInfo._id)) {
                   disabledState = true; 
                   buttonText = 'Joined'; 
-                } else if (club.requests.includes(this.props.currentUserID)) {
+                } else if (club.requested.includes(this.props.userInfo._id)) {
                   disabledState = true; 
                   buttonText = 'Requested'
                 } else {
@@ -76,13 +118,13 @@ class BrowseAllClubs extends React.Component {
                   buttonText = 'Request to join';
                 }
                 return (
-                <Paper elevation={0} variant='outlined' key={club.clubID}> 
+                <Paper elevation={0} variant='outlined' key={club._id}> 
                   <ListItem variant="outlined">
                     <ListItemText
                     primary={club.name}/>
                       <ListItemSecondaryAction>
                         {
-                          club.execs.includes(this.props.currentUserID) || this.props.userIsAdmin ? 
+                          club.execs.includes(this.props.userInfo._id) || this.props.userInfo.permissions === 1 ? 
                             <Link
                             to={{
                               pathname:'/ClubDashboard', 
@@ -90,13 +132,8 @@ class BrowseAllClubs extends React.Component {
                                 club:club
                               }
                             }}
-                            style={{textDecoration:'none'}}> 
-                              <Button
-                              size="small"
-                              edge="end" 
-                              variant="outlined"
-                              color="primary"
-                              style={{marginLeft:'10px'}}> 
+                            className="notUnderlined"> 
+                              <Button size="small" edge="end"  variant="outlined" color="primary" className="button"> 
                                 edit
                               </Button>
                             </Link>
@@ -104,23 +141,23 @@ class BrowseAllClubs extends React.Component {
                         }
                         <Link
                         to={{
-                          pathname:club.link, 
+                          pathname:`/clubs/${club._id}`, 
                           state:{
                             club:club
                           }
                         }}
-                        style={{textDecoration:'none'}}> 
+                        className="notUnderlined"> 
                           <Button
                           size="small"
                           edge="end" 
                           variant="outlined"
                           color="primary"
-                          style={{marginLeft:'10px'}}> 
+                          className="button"> 
                             view
                           </Button>
                         </Link>
                         <Button 
-                            id={club.clubID}
+                            id={club._id}
                             size="small"
                             edge="end" 
                             aria-label="join" 
@@ -128,8 +165,7 @@ class BrowseAllClubs extends React.Component {
                             color="primary"
                             disabled={disabledState}
                             onClick={this.joinRequest}
-                            style={{margin:'10px'}}
-                            >
+                            className="button">
                             {buttonText}
                         </Button>
                       </ListItemSecondaryAction>
@@ -137,7 +173,8 @@ class BrowseAllClubs extends React.Component {
                 </Paper>
                 )})
             }
-          </List>
+            </List>
+          }
         </Container> 
       </div>
     )
