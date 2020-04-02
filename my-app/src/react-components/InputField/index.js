78bc5ee545883f2ec892233228612ce59d31e19e
Zhan Lu <zhanlu0523@gmail.com>
2020-04-02 13:38:34 -0400

    debug

--- my-app/src/react-components/InputField/index.js
+++ my-app/src/react-components/InputField/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import "./style.css"
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
 
 //custom input field class for collecting text input
 class InputField extends React.Component{
     render(){
-        const { classes, label, value, onInputChange, name, type } = this.props;
+        const { label, value, onInputChange, name, type } = this.props;
 
 
         return (
@@ -20,42 +18,13 @@ class InputField extends React.Component{
                     color="white"
                     id="margin-normal"
                     defaultValue={value || ""}
-                    className={classes.root}
                     margin="dense"
                     onChange={onInputChange}
-                    InputLabelProps={{
-                        classes: {
-                          root: classes.inputLabel,
-                          focused: "focused"
-                        }
-                      }}
-                    InputProps={classes.inputLabel}
                 />
             </Grid>
         )
     }
 }
 
-/*
- * Note: there are only two ways to change the color of modules from material-ui
- *       both cannot be done by putting styles in css file
- */
-const styles = () => ({
-    inputLabel: {
-        color: "rgb(f8f9fa)",
-        "&.focused": {
-          color: "white"
-        }
-    },
-    input:{
-        color: "white"
-    },
-    root:{
-        color:"white"
-    }
-})
-InputField.propTypes = {
-  classes: PropTypes.object.isRequired
-};
 
-export default withStyles(styles)(InputField);
+export default InputField
