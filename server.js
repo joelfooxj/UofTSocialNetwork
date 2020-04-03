c7cce6a0b1edb9beb0ae2d5186ba3b57d1992191
nikita-sh <shumeiko.nikita@gmail.com>
2020-04-02 21:10:56 -0400

    Please help me lord

--- server.js
+++ server.js
@@ -166,6 +166,15 @@ module.exports.authAdmin = (req, res, next) => {
     }
 }
 
+/*** Webpage routes below **********************************/
+// Serve the build
+app.use(express.static(__dirname + "/client/build"));
+
+// All routes other than above will go to index.html
+app.get("*", (req, res) => {
+    res.sendFile(__dirname + "/client/build/index.html");
+});
+
 //start server
 const port = process.env.PORT || 5000
 app.listen(port, () => {
