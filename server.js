338da0c53d557f99cedfdac5187b7b896922869b
nikita-sh <shumeiko.nikita@gmail.com>
2020-04-02 22:22:46 -0400

    Changed config files.

--- server.js
+++ server.js
@@ -167,11 +167,11 @@ module.exports.authAdmin = (req, res, next) => {
 }
 
 // Serve the build
-app.use(express.static(__dirname + "/client/public"));
+app.use(express.static(__dirname + "/client/build"));
 
 // All routes other than above will go to index.html
 app.get("*", (req, res) => {
-    res.sendFile(__dirname + "/client/public/index.html");
+    res.sendFile(__dirname + "/client/build/index.html");
 });
 
 //start server
