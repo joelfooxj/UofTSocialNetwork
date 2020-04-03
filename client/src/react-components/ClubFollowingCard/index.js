4c2953ebb8ab9ce61fab88faa3fd84a375010c09
Nikita Shumeiko <36054510+nikita-sh@users.noreply.github.com>
2020-04-03 16:24:20 -0400

    Revert "Revert "Fixed bug in Following page, fixed date sorting for posts""

--- client/src/react-components/ClubFollowingCard/index.js
+++ client/src/react-components/ClubFollowingCard/index.js
@@ -1,54 +1,40 @@
 import React from 'react'
-import {Row, Col, Image, Card, Container, Button} from 'react-bootstrap'
-
+import {Row, Col, Image, Card, Container} from 'react-bootstrap'
+import { Button } from '@material-ui/core'
+import { withRouter } from 'react-router-dom'
+ 
 class ClubFollowingCard extends React.Component{
-
-	state={
-		deleted:false
+	goToClub(e) {
+		e.preventDefault();
+		this.props.history.push(`/club/${this.props.clubID}`)
 	}
 
-	delete(){
-        this.setState({ deleted: true });
-
-    }
-
-
-
-
 	render(){
-		const { clubProfile, clubName, clubFollowing, type } = this.props;
-		const deleteThis = this.state.deleted ? {display:'none'} : {}
 		return (
-		<Container>
-		<Row><Col xl={1} lg={1}></Col><Col>
-		<Card  className="text-center" border="dark">
-		<Card.Body>
 			<Container>
-				<Row>
-					<Col lg={1} xl={1}>
-						<Image width={"50px"} height={"50px"} src={clubProfile}/>
-					</Col>
-					<Col lg={2} xl={2}>
-						<h5>{clubName}</h5>
-					</Col>
-					<Col>
-						<p>Followers: {clubFollowing}</p>
-					</Col>
-					<Col>
-						<p>Member Type: {type}</p>
-					</Col>
-					<Col >
-						<Button onclick={this.state.deleted}>unfollow</Button>
-					</Col>
-				</Row>
+			<Row><Col xl={1} lg={1}></Col><Col>
+			<Card  className="text-center" border="dark">
+			<Card.Body>
+				<Container>
+					<Row>
+						<Col lg={1} xl={1}>
+							<Image width={"50px"} height={"50px"} src={this.props.clubProfile}/>
+						</Col>
+						<Col lg={2} xl={2}>
+							<h5>{this.props.clubName}</h5>
+						</Col>
+						<Col >
+							<Button variant='contained' color='primary' onClick={this.goToClub.bind(this)}>View</Button>
+						</Col>
+					</Row>
 
+				</Container>
+			</Card.Body>
+			</Card></Col>
+			<Col xl={1} lg={1}></Col></Row>
 			</Container>
-		</Card.Body>
-		</Card></Col>
-		<Col xl={1} lg={1}></Col></Row>
-		</Container>
-			);
+		);
 	}
 }
 
-export default ClubFollowingCard;
+export default withRouter(ClubFollowingCard);
